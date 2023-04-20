d310378c96b0cc540a0ad87a9524db295f640363
dayli <dayli@DESKTOP-VP1QNAQ>
2023-04-19 23:19:31 -0600

    Agregar archivos de proyecto.

---
+++ Binary Files/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+FileStream ReaderFile;
+FileStream WriterFile;
+BinaryReader Reader;
+BinaryWriter Writer;
+
+WriterFile = new FileStream("countries.dat",FileMode.OpenOrCreate,FileAccess.Write);
+Writer = new BinaryWriter (WriterFile);
+
+Writer.Write("COUNTRIES");
+Writer.Write("\nItaly");
+Writer.Write("Holand");
+Writer.Write("Thailand");
+Writer.Write("Japan");
+Writer.Write("Germany");
+Writer.Write("Iceland");
+
+Writer.Close();
+WriterFile.Close();
+
+ReaderFile = new FileStream("countries.dat", FileMode.OpenOrCreate, FileAccess.Read);
+Reader = new BinaryReader(ReaderFile);
+
+while (ReaderFile.Position != ReaderFile.Length)
+{
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+    Console.WriteLine(Reader.ReadString());
+}
+Reader.Close();
+ReaderFile.Close();
+Console.ReadKey();
